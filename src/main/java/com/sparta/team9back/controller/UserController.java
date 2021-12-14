bfca3b8a03025320b7d98c7413cf0e750408fcfa
HundredCleanWater <maxmp9904@gmail.com>
2021-12-14 10:14:20 +0900

    유효성 검사 완료

--- src/main/java/com/sparta/team9back/controller/UserController.java
+++ src/main/java/com/sparta/team9back/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.sparta.team9back.controller;
 
 import com.sparta.team9back.dto.User.SignupRequestDto;
+import com.sparta.team9back.dto.User.UserInfoDto;
+import com.sparta.team9back.model.User;
 import com.sparta.team9back.security.UserDetailsImpl;
 import com.sparta.team9back.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -22,13 +24,13 @@ public class UserController {
         userService.registerUser(requestDto);
 
     }
-
-
+    @PostMapping("/userinfo")
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        return new UserInfoDto(user.getUsername(), user.getNickname(), user.getPassword());
+    }
 
 }
-
-
-
 
 
 //}
--- src/main/java/com/sparta/team9back/validator/UserInfoValidator.java
+++ src/main/java/com/sparta/team9back/validator/UserInfoValidator.java
@@ -7,26 +7,27 @@ import java.util.regex.Pattern;
 
 public class UserInfoValidator {
     public static void validateUserInfoInput(SignupRequestDto requestDto) {
-        String patternUsername = "^(.+)@(.+)$";
-//        String patternNickname = "[^!@#$%^&*(),.?\\\":{}|<>]{2,}";
+        String patternUsername = "^[a-zA-Z0-9]{3,20}$";
+        String patternNickname = "^[a-zA-Z0-9]{3,10}$";
+        String patternPassword = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$";
 
         String nickname = requestDto.getNickname();
         String password = requestDto.getPassword();
         String username = requestDto.getUsername();
 
         // 닉네임 형식 확인
-//        if(nickname == null || !Pattern.matches(patternNickname, nickname)) {
-//            throw new IllegalArgumentException("최소 2자 이상, 특수문자는 포함될 수 없습니다.");
-//        }
+        if(nickname == null || !Pattern.matches(patternNickname, nickname)) {
+            throw new IllegalArgumentException(" 3자이상 10자 이하로 가능합니다.");
+        }
 
         // 비밀번호 형식 확인
-        if(password == null || password.length() < 4){
-            throw new IllegalArgumentException("최소 4자 이상이어야 합니다.");
+        if(password == null || !Pattern.matches(patternPassword, password)){
+            throw new IllegalArgumentException("영문 & 숫자 & 특수기호 포함 8자이상 20자이하로 가능합니다.");
         }
 
         // 아이디 형식 검사
-//        if(userId == null || !Pattern.matches(patternUserId, userId)) {
-//            throw new IllegalArgumentException("최소 2자 이상, 특수문자는 포함될 수 없습니다.");
-//        }
+       if(username == null || !Pattern.matches(patternUsername, username)) {
+          throw new IllegalArgumentException("영문포함 3자이상 20자이하 가능합니다.");
+       }
     }
 }
