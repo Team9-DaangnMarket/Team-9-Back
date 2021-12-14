4022e1325c422a33255c907257321139fda550d3
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-12-14 10:36:05 +0900

    1214 paging 세부 부분 각만 잡은 것 - gradle, properties 맞추기

--- src/main/java/com/sparta/team9back/repository/UserRepository.java
+++ src/main/java/com/sparta/team9back/repository/UserRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 닉네임 존재 여부
-    boolean existsByUsername(String username);
 
     Optional<User> findByUsername(String username);
 }
